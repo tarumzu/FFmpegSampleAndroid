5c679ae351469a28f9d356df8abea98f1c787970
tarumzu <wwww.0909mizu@gmail.com>
2019-11-24 20:06:49 +0900

    add FFmpeg

--- app/src/main/java/jp/co/japantaxi/ffmpegsampleandroid/MainActivity.kt
+++ app/src/main/java/jp/co/japantaxi/ffmpegsampleandroid/MainActivity.kt
@@ -2,11 +2,29 @@ package jp.co.japantaxi.ffmpegsampleandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.arthenica.mobileffmpeg.FFmpeg
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FFmpeg.execute("-version")
+        val rc = FFmpeg.getLastReturnCode()
+        val output = FFmpeg.getLastCommandOutput()
+
+        when (rc) {
+            FFmpeg.RETURN_CODE_SUCCESS -> {
+                Log.i("FFmpeg", "Success!")
+            }
+            FFmpeg.RETURN_CODE_CANCEL -> {
+                Log.e("FFmpeg", output)
+            }
+            else -> {
+                Log.e("FFmpeg", output)
+            }
+        }
     }
 }
